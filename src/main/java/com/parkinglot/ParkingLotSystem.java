1a465f6f12e53cd8903f8d43028e53223060129f
vishal-patil01 <vishalpatil01@hotmail.com>
2020-03-22 10:56:29 +0530

    ADDED : Test Case & Code To UnPark Vehicle in ParkingLot

--- src/main/java/com/parkinglot/ParkingLotSystem.java
+++ src/main/java/com/parkinglot/ParkingLotSystem.java
@@ -3,7 +3,15 @@ package com.parkinglot;
 public class ParkingLotSystem {
     private Object vehicle;
 
-    public boolean parkVehicle(Object vehicle) {
+    public boolean park(Object vehicle) {
+        this.vehicle=vehicle;
         return true;
+    }
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle=null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/parkinglot/TestParkingLot.java
+++ src/test/java/com/parkinglot/TestParkingLot.java
@@ -6,18 +6,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class TestParkingLot {
-        ParkingLotSystem parkingLotSystem;
+    ParkingLotSystem parkingLotSystem;
 
-        @Before
-        public void setup() {
-            parkingLotSystem = new ParkingLotSystem();
-        }
-
-        @Test
-        public void givenVehicle_WhenParked_ShouldReturnTrue() {
-            boolean isParked = parkingLotSystem.parkVehicle(new Object());
-            assertTrue(isParked);
-        }
+    @Before
+    public void setup() {
+        parkingLotSystem = new ParkingLotSystem();
     }
+
+    @Test
+    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+        boolean isParked = parkingLotSystem.park(new Object());
+        assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        Object vehicle =new Object();
+        parkingLotSystem.park(vehicle);
+        boolean isUnParked = parkingLotSystem.unPark(vehicle);
+        assertTrue(isUnParked);
+    }
+}
 
 
